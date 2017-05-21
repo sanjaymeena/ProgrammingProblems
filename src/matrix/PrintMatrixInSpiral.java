ad56a42a8db4806b8d4e78b5a082b33c65620390
Sanjay <sanjaymeena@gmail.com>
2017-05-21 11:20:40 +0800

    added 1st commit

---
+++ src/matrix/PrintMatrixInSpiral.java
@@ -0,0 +1,81 @@
+package matrix;
+
+public class PrintMatrixInSpiral {
+
+	void printMatrixClockwise(int numbers[][]) {
+		int rows = numbers.length;
+		int columns = numbers[0].length;
+
+		int start = 0;
+
+		while (columns > start * 2 && rows > start * 2) {
+			printRing(numbers, start);
+			++start;
+
+		}
+
+	}
+
+	private void printRing(int[][] numbers, int start) {
+		// TODO Auto-generated method stub
+		int rows = numbers.length;
+		int columns = numbers[0].length;
+
+		int endX = columns - 1 - start;
+		int endY = rows - 1 - start;
+
+		// Print a row from left to right
+		for (int i = start; i <= endX; ++i) {
+			int number=numbers[start][i];
+			printNumber(number);
+		}
+
+		// Print a column top down
+		if(start < endY){
+			for (int i = start+1; i <= endY; ++i) {
+				int number=numbers[i][endX];
+				printNumber(number);
+			}
+		}
+		
+		//print a row from right to left
+		if(start < endX && start< endY){
+			for (int i = endX-1; i >= start; --i) {
+				int number=numbers[endY][i];
+				printNumber(number);
+			}
+		}
+		
+		// print a column bottom up
+		if(start < endX && start< endY-1){
+			for (int i = endY-1; i >= start+1; --i) {
+				int number=numbers[i][start];
+				printNumber(number);
+			}
+		}
+
+	}
+
+	private void printNumber(int number) {
+		// TODO Auto-generated method stub
+		System.out.print(number + " ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintMatrixInSpiral pmi=new PrintMatrixInSpiral();
+		pmi.test();
+	}
+
+	/**
+	 * 
+	 */
+	private void test() {
+		 int arrA[][] = { { 1, 2, 3, 4, 5 }, { 18, 19, 20, 21, 6 },
+				{ 17, 28, 29, 22, 7 }, { 16, 27, 30, 23, 8 },
+				{ 15, 26, 25, 24, 9 }, { 14, 13, 12, 11, 10 } };
+		 printMatrixClockwise(arrA);
+		 
+	}
+
+}
---
+++ src/trees/SymmetricalBinaryTrees.java
@@ -0,0 +1,142 @@
+/**
+ * 
+ */
+package trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import trees.TreeTraversal.Node;
+
+/**
+ * @author Sanjay
+ *
+ */
+public class SymmetricalBinaryTrees {
+
+	public class Node<T> {
+		public T data;
+		public Node<T> left;
+		public Node<T> right;
+
+		public Node(T num) {
+			this.data = num;
+			this.left = null;
+			this.right = null;
+		}
+
+		public Node() {
+			this.left = null;
+			this.right = null;
+		}
+
+	}
+
+	public boolean isMirrorTree(Node<?> tree, Node<?> mirrorTree) {
+		if (null == tree && null == mirrorTree)
+			return true;
+
+		if (null == tree || null == mirrorTree)
+			return false;
+
+		if (tree.data != mirrorTree.data)
+			return false;
+
+		if (false == isMirrorTree(tree.left, mirrorTree.right))
+			return false;
+
+		if (false == isMirrorTree(tree.right, mirrorTree.left))
+			return false;
+
+		return true;
+	}
+
+	public void printTree(Node<?> root) {
+		if (root == null) {
+			System.out.println("Tree is empty");
+			return;
+		}
+		Queue<Node<?>> queue = new LinkedList<Node<?>>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			Node<?>  node = queue.poll();
+			System.out.printf("%d ", node.data);
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+		}
+		System.out.println("");
+		return;
+	}
+
+	private void test() {
+		// TODO Auto-generated method stub
+		// Binary Tree 1:
+		// root level 0
+		Node<Integer> A = new Node<Integer>(70);
+		// Level 1
+		Node<Integer> B = new Node<Integer>(50);
+		Node<Integer> C = new Node<Integer>(55);
+
+		// Level 2
+		Node<Integer> D = new Node<Integer>(25);
+
+		Node<Integer> E = new Node<Integer>(80);
+		Node<Integer> F = new Node<Integer>(37);
+		Node<Integer> G = new Node<Integer>(45);
+
+		// connect Level 0 and 1
+		A.left = B;
+		A.right = C;
+		// connect level 1 and level 2
+		B.left = D;
+		B.right = E;
+		C.left = F;
+		C.right = G;
+
+		// Binary Tree 2:
+
+		// root level 0
+		Node<Integer> P = new Node<Integer>(70);
+		// Level 1
+		Node<Integer> Q = new Node<Integer>(55);
+		Node<Integer> R = new Node<Integer>(50);
+		// Level 2
+		Node<Integer> S = new Node<Integer>(45);
+		Node<Integer> T = new Node<Integer>(37);
+		Node<Integer> U = new Node<Integer>(80);
+		Node<Integer> V = new Node<Integer>(25);
+
+		// connect Level 0 and 1
+		P.left = Q;
+		P.right = R;
+		// connect level 1 and level 2
+		Q.left = S;
+		Q.right = T;
+		R.left = U;
+		R.right = V;
+
+		System.out.println("Binary Tree 1 :");
+		printTree(A);
+		System.out.println("Binary Tree 2 :");
+		printTree(P);
+
+		boolean isMirrorBinaryTrees = isMirrorTree(A, P);
+		System.out.printf("Check Trees are mirror binary tree : %b", isMirrorBinaryTrees);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SymmetricalBinaryTrees sbt = new SymmetricalBinaryTrees();
+		sbt.test();
+
+	}
+
+}
